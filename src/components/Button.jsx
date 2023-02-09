4951944cb8ba52a3ce05b4c470a511142d50ea30
Dillon Ellis <dillonzellis@gmail.com>
2023-02-08 22:17:08 -0500

    button shadow

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ function Button({ btnLink, btnText }) {
   return (
     <div>
       <a
-        className="inline-flex rounded-full bg-accent-400 px-4 py-2 font-bold capitalize"
+        className="btn-shadow inline-flex rounded-full bg-accent-400 px-4 py-2 font-bold capitalize"
         href={btnLink}
         target="_blank"
         rel="noopener noreferrer"
